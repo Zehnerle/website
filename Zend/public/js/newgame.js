fb0f077b29c587e55fef4217712c812c1c11962d
ursi <ursi@server.fake>
2013-05-22 23:20:00 +0200

    some corrections

--- Zend/public/js/newgame.js
+++ Zend/public/js/newgame.js
@@ -1,5 +1,5 @@
 
-/* von Gruppe 2 ausgeliehen */
+/* Danke Gruppe 2! */
 
 
 function reply_click(id) {
@@ -12,51 +12,54 @@ function submitCheck() {
 	var returnval = true;
 	var form = document.forms[0];
 	
-	if(form.player1.value === ''){
+	if(form.player1.value === '') {
 		returnval = false;
-		form.player1.parentNode.parentNode.setAttribute('class','control-group error');
-		var s = form.player1.parentNode.parentNode.childNodes[0];
-		s.innerHTML = 'Player1 name field is missing!';
+		player1 = form.player1.parentNode.parentNode;
+		player1.setAttribute('class','control-group error');
+		player1.childNodes[0].innerHTML = 'Player1 name field is missing!';
 	} else {
-		form.player1.parentNode.setAttribute('class','');
-		form.player1.parentNode.parentNode.childNodes[0] = '';
+		player1 = form.player1.parentNode;
+		player1.setAttribute('class','');
+		player1.parentNode.childNodes[0] = '';
 	}
-	
-	if(form.player2.value === ''){
+		   
+	if(form.mail1.value === '' || !checkEmailFormat(form.mail1.value)) {
 		returnval = false;
-		form.player2.parentNode.parentNode.setAttribute('class','control-group error');
-		var s = form.player2.parentNode.parentNode.childNodes[0];
-		s.innerHTML = 'Player2 name field is missing!';
-	} else{
-		form.player2.parentNode.setAttribute('class','');
-		form.player2.parentNode.parentNode.childNodes[0] = '';
-	}
-	   
-	if(form.mail1.value === '' || !checkEmailFormat(form.mail1.value)){
-		returnval = false;
-		form.mail1.parentNode.parentNode.setAttribute('class','control-group error');
-		var s = form.mail1.parentNode.parentNode.childNodes[0];
-		s.innerHTML = 'Player1 mail is missing';
-	} else{
-		form.mail1.parentNode.setAttribute('class','');
-		form.mail1.parentNode.parentNode.childNodes[0]='';
+		mail1 = form.mail1.parentNode.parentNode;
+		mail1.setAttribute('class','control-group error');
+		mail1.childNodes[0].innerHTML = 'Player1 mail is missing';
+	} else {
+		mail1 = form.mail1.parentNode;
+		mail1.setAttribute('class','');
+		mail1.parentNode.childNodes[0]= '';
 
 	}
 	
-	if(form.mail2.value === '' || !checkEmailFormat(form.mail2.value)){
+	if(form.player2.value === '') {
 		returnval = false;
-		form.mail2.parentNode.parentNode.setAttribute('class','control-group error');
-		var s = form.mail2.parentNode.parentNode.childNodes[0];
-		s.innerHTML = 'Player2 mail is missing';
-	} else{
-		form.mail2.parentNode.setAttribute('class','');
-		form.mail2.parentNode.parentNode.childNodes[0]='';
+		player2 = form.player2.parentNode.parentNode;
+		player2.setAttribute('class','control-group error');
+		player2.childNodes[0].innerHTML = 'Player2 name field is missing!';
+	} else {
+		player2 = form.player2.parentNode;
+		player2.setAttribute('class','');
+		player2.parentNode.childNodes[0] = '';
+	}
+	
+	if(form.mail2.value === '' || !checkEmailFormat(form.mail2.value)) {
+		returnval = false;
+		mail2 = form.mail2.parentNode.parentNode;
+		mail2.setAttribute('class','control-group error');
+		mail2.childNodes[0].innerHTML = 'Player2 mail is missing';
+	} else {
+		mail2 = form.mail2.parentNode;
+		mail2.setAttribute('class','');
+		mail2.parentNode.childNodes[0]= '';
 	}
 	   
-	if(form.choice1.value === undefined || form.choice1.value === '' || form.choice1.value === null ){
+	if(form.choice1.value === undefined || form.choice1.value === '' || form.choice1.value === null ) {
 		returnval = false;
-		var el = document.getElementById('weapons');
-		el.innerHTML = 'Your choice is missing!';
+		document.getElementById('weapons').innerHTML = 'Your choice is missing!';
 	}
 	
 	return returnval;
--- Zend/public/js/player2game.js
+++ Zend/public/js/player2game.js
@@ -1,5 +1,6 @@
 
-/* von Gruppe 2 ausgeliehen */
+/* Danke Gruppe 2! */
+
 
 function reply_click(id) {
 	var x = document.forms[0].choice2;
@@ -10,15 +11,16 @@ function submitCheck() {
 
 	var returnval = true;
 	var form = document.forms[0];
-
-	if(form.mail2.value === ''){
+	
+	if(form.mail2.value === '') {
 		returnval = false;
-		form.mail2.parentNode.parentNode.setAttribute('class','control-group error');
-		var s = form.mail2.parentNode.parentNode.childNodes[0];
-		s.innerHTML = 'Mail is missing';
-	} else{
-		form.mail2.parentNode.setAttribute('class','');
-		form.mail2.parentNode.parentNode.childNodes[0]='';
+		mail2 = form.mail2.parentNode.parentNode;
+		mail2.setAttribute('class','control-group error');
+		mail2.childNodes[0].innerHTML = 'Mail is missing';
+	} else {
+		mail2 = form.mail2.parentNode;
+		mail2.setAttribute('class','');
+		mail2.parentNode.childNodes[0]= '';
 	}
 	   
 	if(form.choice2.value === undefined || form.choice2.value === '' || form.choice2.value === null ){
